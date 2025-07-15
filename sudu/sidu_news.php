51f157b0cd322b976428803709e4fe24a4662d80
gladson-mathew <114560251+gladson-mathew@users.noreply.github.com>
2025-07-15 12:36:04 +0530

    Add files via upload

---
+++ sudu/sidu_news.php
@@ -0,0 +1,6 @@
+Enjoy SIDU
+
+<br>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle" style="display:inline-block;width:300px;height:250px" data-ad-client="ca-pub-5882057161333248" data-ad-slot="8520217086"></ins>
+<script>(adsbygoogle=window.adsbygoogle || []).push({});</script>
